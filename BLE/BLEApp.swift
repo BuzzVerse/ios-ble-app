89a84e17f7d42482a7ec7a40ae1476aeeb742e93
wybran <krystian.wybranowski@gmail.com>
2024-06-29 20:48:49 +0200

    Initial Commit

---
+++ BLE/BLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEApp.swift
+//  BLE
+//
+//  Created by Krystian Wybranowski on 29/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
